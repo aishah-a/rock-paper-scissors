b5cace2f37da5b393440c2b65ee9e0f95b3dc8aa
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-03-16 18:16:37 -0600

    Add game function.

--- test_script.js
+++ test_script.js
@@ -10,10 +10,6 @@ function getPlayerChoice(){
     return playerChoice;
 }
 
-const computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-let gamePlay;
-
 function checkInput(choiceP) {
     if (
     (playerSelection === 'rock') ||
@@ -30,6 +26,40 @@ function checkInput(choiceP) {
     }
 }
 
-checkInput(playerSelection);
-console.log(gamePlay);
+function checkWinner(choiceP, choiceC) {
+    if (computerSelection === playerSelection) {
+    return "The computer chose " + computerSelection + " too. It's a tie!";
+    } else if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'paper' && playerSelection === 'rock') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper')) {
+    return 'Computer played "' + computerSelection + '". The computer wins! You lose!';
+    } else if (
+    (computerSelection === 'rock' && playerSelection === 'paper') ||
+    (computerSelection === 'paper' && playerSelection === 'scissors') ||
+    (computerSelection === 'scissors' && playerSelection === 'rock')) {
+    return 'Computer played "' + computerSelection + '".  You win!';
+    }
+}
+
+function game () {
+    checkInput(playerSelection);
+    console.log(gamePlay);
+    if (gamePlay = true) {
+        checkWinner(playerSelection, computerSelection);
+        let roundWinner = (checkWinner());
+        console.log(roundWinner);
+    }
+
+}
+
+const computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+let gamePlay;
+game();
+
+// get choices
+// check input
+// if input valid and gamplay = true, check winner
+// print winner
 
